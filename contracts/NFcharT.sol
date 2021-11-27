02d40caf3efe4a99d05031d6759b35730dcaf8dd
Dallon Asnes <dallon.asnes@gmail.com>
2021-11-27 14:33:27 -0600

    Setup customizable lookback window and fetch from oracle in loop

--- contracts/NFcharT.sol
+++ contracts/NFcharT.sol
@@ -7,7 +7,12 @@ import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
 
 // Source: https://andrecronje.medium.com/easy-on-chain-oracles-54d82961a2a0
 interface v3oracle {
-    function assetToAsset(address, uint, address, uint) external view returns (uint);
+    function assetToAsset(
+        address,
+        uint256,
+        address,
+        uint256
+    ) external view returns (uint256);
 }
 
 contract NFcharT is ERC721Enumerable, Ownable, ReentrancyGuard {
@@ -15,12 +20,12 @@ contract NFcharT is ERC721Enumerable, Ownable, ReentrancyGuard {
     using Strings for uint256;
 
     v3oracle constant oracle = v3oracle(0x0F1f5A87f99f0918e6C81F16E59F3518698221Ff);
-    
+
     // state vars
     bool public paused = true;
-    uint private lookBackWindow = 1; // in days... TODO: is this the place to put it for customizable by each user
     mapping(bytes => bool) public tokenPairExistenceMapping; // the key is a concatenation of token0 and token1
     mapping(uint256 => address[]) internal tokenIdToTokenPairMapping; // the key is tokenId and value is array of addresses for two tokens being tracked
+    mapping(uint256 => uint256) internal lookBackWindowForToken; // key is tokenId and value is lookback window (in days) set for that token
 
     // constructor
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
@@ -61,24 +66,27 @@ contract NFcharT is ERC721Enumerable, Ownable, ReentrancyGuard {
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         // fetch two tokens being compared by tokenId key
         address[] memory tokens = tokenIdToTokenPairMapping[tokenId];
-        // TODO: get TWAP period from web client? 
+        // TODO: get TWAP period from web client?
         // now query Uniswap Oracle for Price Data (https://andrecronje.medium.com/easy-on-chain-oracles-54d82961a2a0)
         // TODO: need a loop to get these over different interval
         // eg - to get over 24 hours period, need to query for 30 mins, then 60 mins, then 90, then ... up to 24 hrs
         // and return all values as an array to plot
 
-        uint _twip = oracle.assetToAsset(tokens[0], 1e18, tokens[1], 3600); // over the last 30 mins?
-        if (_twip == 0){
-            // TODO: remove me. this is dummy logic to get the compiler to not complain about unused variable
+        uint256 twipCountToFetch = lookBackWindowForToken[tokenId] * 48; // 48 comes from assuming 3600 is for 30 mins as docs say. and there are 48 periods of 30 mins in one day
+        uint256[] memory twips = new uint256[](twipCountToFetch);
+        for (uint256 i = 0; i < twipCountToFetch; i++) {
+            uint256 currentLookbackWindow = (i + 1) * 3600; // recall i is 0 indexed
+            uint256 twip = oracle.assetToAsset(tokens[0], 1e18, tokens[1], currentLookbackWindow);
+            twips[i] = twip;
         }
 
         // TODO
         // Create JSON template
         // Create SVG Template
-            // Query Uniswap Oracle for Price Data
+        // Query Uniswap Oracle for Price Data
         // base64 encode the JSON blob
         // return encoded JSON
-        return "";
+        return '';
     }
 
     /**
@@ -89,10 +97,23 @@ contract NFcharT is ERC721Enumerable, Ownable, ReentrancyGuard {
     }
 
     /**
+     * @dev Sets lookback window for tokenId
+     */
+    function setLookbackWindow(uint256 tokenId, uint256 dayCount) public {
+        lookBackWindowForToken[tokenId] = dayCount;
+    }
+
+    /**
+     * @dev Gets lookback window for tokenId
+     */
+    function getLookbackWindow(uint256 tokenId) public view returns (uint256) {
+        return lookBackWindowForToken[tokenId];
+    }
+
+    /**
      * @dev Sends the ETH balance in the contract to the contract owner.
      */
     function withdrawAll() public payable onlyOwner {
         require(payable(msg.sender).send(address(this).balance));
     }
-
 }
